a5ab0695fa66b1f12b9533305a7049fdbdf39f8b
kerusu <kerusu1017@yahoo.co.jp>
2022-10-17 12:44:09 +0900

    DrawCanvas削除

--- pages/shadow-box/index.tsx
+++ pages/shadow-box/index.tsx
@@ -1,11 +1,19 @@
-import { DrawCanvas } from "../../components/DrawCanvas";
 import { Model } from "../../components/Model";
+import * as Fiber from "@react-three/fiber";
+import * as Drei from "@react-three/drei";
+import * as React from "react";
 
 const Shadowbox = () => {
   return (
     <div>
       <main>
-        <DrawCanvas />
+        <div style={{ width: "100vw", height: "100vh" }}>
+          <Fiber.Canvas camera={{ position: [50, 10, 0], fov: 15 }}>
+            <Drei.OrbitControls />
+            <ambientLight />
+            <Model src="/shadowbox/rowlet/rowlet.gltf" />
+          </Fiber.Canvas>
+        </div>
       </main>
     </div>
   );
