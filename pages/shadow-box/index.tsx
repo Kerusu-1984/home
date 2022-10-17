5db9964f696aadfeb52f6781bb7c7af33598d8d2
kerusu <kerusu1017@yahoo.co.jp>
2022-10-17 15:01:48 +0900

    説明を追加した

--- pages/shadow-box/index.tsx
+++ pages/shadow-box/index.tsx
@@ -1,18 +1,24 @@
 import { Model } from "../../components/Model";
+import "bootstrap/dist/css/bootstrap.min.css";
 import * as Fiber from "@react-three/fiber";
 import * as Drei from "@react-three/drei";
 import * as React from "react";
 
 const Shadowbox = () => {
   return (
-    <div>
+    <div className="container">
       <main>
-        <div style={{ width: "100vw", height: "100vh" }}>
+        <div style={{ width: "90vw", height: "90vh" }}>
           <Fiber.Canvas camera={{ position: [50, 10, 0], fov: 15 }}>
             <Drei.OrbitControls />
             <ambientLight />
             <Model src="/shadowbox/rowlet/rowlet.gltf" />
           </Fiber.Canvas>
+        </div>
+
+        <div className="d-none d-md-block text-center text-black-50">
+          ドラッグで回転 マウスホイールで拡大縮小
+          右クリックしながらドラッグで移動
         </div>
       </main>
     </div>
