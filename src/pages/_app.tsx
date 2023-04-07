ea127c908e7ae1d58cca5162aa2473f7cf62f804
kerusu <kerusu1017@yahoo.co.jp>
2023-04-08 00:16:25 +0900

    共有用設定

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,6 +8,10 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         <title>kerusu HomePage</title>
         <link rel="icon" href="/favicon.ico" />
+        <meta name="twitter:card" content="summary" />
+        <meta property="twitter:domain" content="kerusu.xyz" />
+        <meta property="og:image" content="kerusu.xyz/img/rowlet.jpg" />
+        <meta property="og:title" content="kerusu HomePage" />
       </Head>
       <Component {...pageProps} />
     </>
