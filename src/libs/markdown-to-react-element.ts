8ce4864b83c22637d92b1c4ec56a1179059fd4b6
kerusu <kerusu1017@yahoo.co.jp>
2023-04-13 19:31:33 +0900

    直接描いたリンクをaタグに変換

--- src/libs/markdown-to-react-element.ts
+++ src/libs/markdown-to-react-element.ts
@@ -5,12 +5,14 @@ import remarkRehype from "remark-rehype";
 import rehypeRaw from "rehype-raw";
 import rehypeStringify from "rehype-stringify";
 import rehypeReact from "rehype-react";
+import remarkGfm from "remark-gfm";
 import { Fragment,createElement } from "react";
 import CustomLink from "../components/CustomLink";
 import CustomImage from "../components/CustomImage";
 
 export const markdownToReactElement = (markdownContent: string) => {
   return remark()
+    .use(remarkGfm)
     .use(remarkBreaks)
     .use(remarkRehype, {
       allowDangerousHtml: true,
